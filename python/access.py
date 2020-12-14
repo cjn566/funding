b7c7c5139103dc531fdd017a704baaf8d1129e09
Colten <cjn566@gmail.com>
2020-12-14 10:00:06 -0800

    python stuff

--- python/access.py
+++ python/access.py
@@ -1,33 +1,37 @@
-import unicornhat as corn
+#import unicornhat as corn
 
 import time
-import http.client
+import requests
+import logging
 
-#conn = http.client.HTTPConnection('localhost:3000')
-#conn.request("GET","/api/student/12")
-#r1 = conn.getresponse()
-#data = r1.read()
-#print(data)
+import http.client
+http.client.HTTPConnection.debuglevel = 1
+
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 red = (255,0,0)
 green = (0,255,0)
 
-def verify(valid):
-  corn.set_all(green if valid else red)
-  corn.show()
-  time.sleep(0.5)
-  corn.clear()
-  corn.show()
+def checkid(id):
+  r = requests.get('http://127.0.0.1:3000/api/student/' + id)
+  print(r.text)
+
+
+#def verify(valid):
+  #corn.set_all(green if valid else red)
+  #corn.show()
+  #time.sleep(0.5)
+  #corn.clear()
+  #corn.show()
 
 running  = True
 
 while running:
     id = input("Student ID: ")
-    if id == "0":
-      verify(True)
-    elif id == "1":
-        verify(False)
-    elif id == "exit":
-        running = False
-    else:
-        print("Invalid Input")
+    checkid(id)
+    #verify(isGood)
